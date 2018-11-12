1becdf1fd56b17f3530ccefddc89f7a7d80e3532
Fernando Zeferino <fe_lz@hotmail.com>
2018-11-12 15:22:42 -0200

    Filtro

--- js/filtra.js
+++ js/filtra.js
@@ -4,12 +4,14 @@ console.log(campoFiltro);
 
 campoFiltro.addEventListener("input", function(){
     var pacientes = document.querySelectorAll(".paciente");
-    if (this.value.length > 0){
+    if (this.value.length > 0) {
         for (var i = 0; i < pacientes.length; i++) {
             var paciente = pacientes[i];
             var tdNome = paciente.querySelector(".info-nome");
             var nome = tdNome.textContent;
-            if (nome != this.value) {
+            var expressao = new RegExp(this.value, "i"); 
+
+            if (!expressao.test(nome)) {
                 paciente.classList.add("invisivel");
             } else {
                 paciente.classList.remove("invisivel");
